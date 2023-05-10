fdcb4039c539077df8fb83e5607ff8e8479d9696
Gabriel Maia <gabriel.gnm@gmail.com>
2023-05-09 22:34:58 -0300

    update login- html files

---
+++ src/js/cadastro.js
@@ -0,0 +1,61 @@
+//Cria Usuario
+
+const cadastroBtn = document.getElementById('cadastroBtn');
+cadastroBtn.addEventListener('click', function createUser(event) {
+	event.preventDefault();
+
+	const nameInput = document.getElementById('name');
+	const emailInput = document.getElementById('email');
+	const passwordInput = document.getElementById('password');
+	const confirmPassword = document.getElementById('confirmPassword');
+	const typeInput = document.getElementById('type');
+
+
+	if (passwordInput.value !== confirmPassword.value) {
+		alert('As senhas não coincidem!');
+	} else {
+		alert(`Cadastro realizado com sucesso!\nNome: ${nameInput.value}\nE-mail: ${emailInput.value}`);
+		const newUser = {
+			name: nameInput.value,
+			email: emailInput.value,
+			type: typeInput.value,
+			password: passwordInput.value,
+		};
+		var usersJSON = localStorage.getItem('users');
+		var users = JSON.parse(usersJSON);
+		if (Array.isArray(users)) {
+			// Se users for um array, push o novo objeto user
+			users.push(newUser);
+		  } else {
+			// Se users não for um array, Cria um array com o novo objeto dentro
+			users = [newUser];
+		  }
+		  // Armazene a lista de objetos no localStorage
+		  localStorage.setItem('users', JSON.stringify(users));
+
+		
+
+		window.location.href = './login.html';
+
+	}
+});
+
+//Deleta Usuario
+function deleteUser(index) {
+	usersList.splice(index, 1);
+	renderUsers();
+}
+
+//Cancela Tela
+document.getElementById('cancelBtn').addEventListener('click', function () {
+	document.getElementById('name').removeAttribute('required');
+	document.getElementById('email').removeAttribute('required');
+	document.getElementById('password').removeAttribute('required');
+	document.getElementById('confirmPassword').removeAttribute('required');
+	window.location.href = './index.html';
+});
+
+
+
+
+
--- src/js/rightBar.js
+++ src/js/rightBar.js
@@ -2,12 +2,25 @@
 const rightButton = document.querySelector(".right-button");
 rightButton.addEventListener("click", function () {
 
+  
+
   const rightBar = document.querySelector(".right-bar");
   const currentBar = parseInt(getComputedStyle(rightBar).right);
   if (currentBar === 0) {
     rightBar.style.right = '-600px';;
   } else {
     rightBar.style.right = "0px";
+  }
+
+  if (sessionStorage.length !== 0) {
+    //Acessa o Session storage
+    var userJSON = sessionStorage.getItem('user');
+    var user = JSON.parse(userJSON);
+  
+  
+    //altera HTML do RightBar
+    var tituloRightBar = document.getElementById("tituloRightBar");
+    tituloRightBar.textContent = "Bem Vindo " + user.name;
   }
 });
 
@@ -21,46 +34,4 @@ button.addEventListener('click', function () {
   } else {
     image.src = './img/perfil-de-usuario.png';
   }
-});
-//Faz Tela de Login na RightBar
-const loginBtn = document.getElementById('loginBtn');
-loginBtn.addEventListener('click', function () {
-  fetch("login.html")
-    .then(response => response.text())
-    .then(data => {
-      const telaLogin = document.getElementById("loginCadastroBtn");
-      telaLogin.innerHTML = data;
-    })
-    .catch(error => console.log(error));
-});
-//Faz Tela de Cadastro na RightBar
-const cadastroBtn = document.getElementById('cadastroBtn');
-cadastroBtn.addEventListener('click', function () {
-  fetch("cadastro.html")
-    .then(response => response.text())
-    .then(data => {
-      const telaCadastro = document.getElementById("loginCadastroBtn");
-      telaCadastro.innerHTML = data;
-    })
-    .catch(error => console.log(error));
-});
-
-//Cancela Tela de Login na RightBar
-// const cancelBtn = document.getElementById('cancelBtn');
-// cancelBtn.addEventListener('click', function() {
-//       history.back();
-//     });
-
-const cancelBtn = document.getElementsByClassName('cancelBtnData');
-cancelBtn.addEventListener('click', function () {
-  fetch("login.html")
-    .then(response => response.text())
-    .then(data => {
-      const telaLogin = document.getElementById("loginCadastroBtn");
-      telaLogin.innerHTML = data;
-
-      // Go back to previous page after updating content
-      history.back();
-    })
-    .catch(error => console.log(error));
 });
